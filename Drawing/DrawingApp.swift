d479070ff2febb1fc739f93556edc027ed15aadd
meitarbasson <meitarbass@gmail.com>
2022-07-21 00:34:46 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Meitar Basson on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
