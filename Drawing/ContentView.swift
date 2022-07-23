7c4d365564eb61f646dcdf949e1eda676d0be806
meitarbasson <meitarbass@gmail.com>
2022-07-23 10:30:35 +0300

    Day 45.

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,29 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var petalOffset = -20.0
-//    @State private var petalWidth = 100.0
-    
-    @State private var colorCycle = 0.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        VStack {
-//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .fill(.red, style: FillStyle(eoFill: true))
-//
-//            Text("Offset")
-//            Slider(value: $petalOffset, in: -40...40)
-//                .padding([.horizontal,.bottom])
-//
-//            Text("Width")
-//            Slider(value: $petalWidth, in: 0...100)
-//                .padding(.horizontal)
-            
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
-            
-            Slider(value: $colorCycle)
-        }
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
+            }
     }
 }
 
---
+++ Drawing/Trapezoid.swift
@@ -0,0 +1,29 @@
+//
+//  Trapezoid.swift
+//  Drawing
+//
+//  Created by Meitar Basson on 23/07/2022.
+//
+
+import SwiftUI
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
